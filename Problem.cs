9fb45ec4c85cfc6d60e7640239cec2f9eed9a9e5
gusdlfrns <103507481+gusdlfrns@users.noreply.github.com>
2025-02-03 18:24:11 +0900

    풀이

--- Problem.cs
+++ Problem.cs
@@ -1,51 +1,50 @@
-using Sysyem;
+using System;
+using System.Linq;
 
-Console.Write("Write the Word : ");
-string word = string.Parse(Console.ReadLine);
-
-int maxLives = 7;
-int currentLives=maxLives;
-
-bool win = false;
-
-List<char> guessedLetters = new List<char>();
-
-while(currentLives > 0 && !win)
+class HangmanGame
 {
-    foreach(char c in word)
+    static void Main()
     {
-        if (guessedLetters.Contains(c))
-        Console.write(c);
-        else
-        Console.write(_);
+        string[] wordList = { "Microsoft", "Hangman", "Programming", "Developer", "Artificial" };
+        Random random = new Random();
+        string answer = wordList[random.Next(wordList.Length)].ToLower(); // 랜덤 단어 선택 (소문자로 변환)
+        char[] display = new string('_', answer.Length).ToCharArray(); // 언더바로 초기화
+        bool[] revealed = new bool[answer.Length]; // 문자 활성화 여부
+        int remaining = answer.Length;
+
+        Console.WriteLine("행맨 게임 시작!");
+        
+        while (remaining > 0)
+        {
+            Console.WriteLine("\n현재 상태: " + string.Join(" ", display));
+            Console.Write("문자 입력: ");
+            char input = char.ToLower(Console.ReadKey().KeyChar); // 소문자로 변환
+            Console.WriteLine();
+
+            if (!char.IsLetter(input))
+            {
+                Console.WriteLine("알파벳만 입력하세요.");
+                continue;
+            }
+
+            bool found = false;
+            for (int i = 0; i < answer.Length; i++)
+            {
+                if (!revealed[i] && answer[i] == input)
+                {
+                    display[i] = answer[i]; 
+                    revealed[i] = true;
+                    found = true;
+                    remaining--;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("틀렸습니다! 다시 시도하세요.");
+            }
+        }
+
+        Console.WriteLine("\n축하합니다! 정답은 '" + answer + "' 입니다!");
     }
-    Console.WriteLine("\nGuess the letter : ");
-    Console.WriteLine(currentLives + "/" + maxLives + " lives remaining.");
-
-    char guess = Convert.ToChar(Console.ReadLine());
-
-    if(word.Contains(guess) && !guessedLetters.Contains(guess))
-    {
-        Console.WriteLine("Correct!");
-    }
-    else
-    {
-        Console.WriteLine("Incorrect!");
-        currentLives--;
-    }
-    guessedLetters.Add(guess);
-
-    bool wordComplete=true;
-
-    foreach(char c in word)
-    if(!guessedLetters.Contains(c))
-    wordComplete=false;
-
-    win = wordComplete;
-}
-if(win){
-    Console.WriteLine("Congraguration. You Win!")
-}
-else{
-    Console.WriteLine("You lose...");
 }
--- Problem9.cs
+++ Problem9.cs
@@ -1,1 +1,31 @@
 using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int[] answer = Enumerable.Range(1, 9).OrderBy(_ => random.Next()).Take(4).ToArray();
+
+        while (true)
+        {
+            Console.Write("숫자 입력 (4개): ");
+            int[] guess = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int strike = 0, ball = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                if (answer[i] == guess[i]) strike++;
+                else if (answer.Contains(guess[i])) ball++;
+            }
+
+            Console.WriteLine($"{strike} Strike, {ball} Ball");
+            if (strike == 4)
+            {
+                Console.WriteLine("정답입니다!");
+                break;
+            }
+        }
+    }
+}
